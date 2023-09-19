3aecaeb748d424b35bc7d1761842e7476925d35a
winniekiarie <kiariebsc@gmail.com>
2023-09-19 20:28:00 +0000

    console v2

--- models/state.py
+++ models/state.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""State Module for HBNB project"""
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
 
 
-class State(BaseModel, Base):
-    """State Module for HBNB project
-    Attributes:
-        name: input name
+class State(Base):
     """
-    __tablename__ = "states"
+    Class with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
