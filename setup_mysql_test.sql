7d11a0597502640a69e4f3f100081050f34e2629
misskabii <shazmakesh254@gmail.com>
2023-10-05 09:01:30 -0700

    static deploy

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,12 @@
--- Prepares a MySQL Test server for the project.
+-- Check if the database hbnb_test_db already exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create a user hbnb_test with the password hbnb_test_pwd in localhost
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
